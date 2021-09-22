bcc94f1278a591187a5854dcc61d62466d1ea1d3
tucno21 <carlitostucno@gmail.com>
2021-09-22 16:29:15 -0500

    modelo unidad

---
+++ app/Models/UnidadesModel.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UnidadesModel extends Model
+{
+    protected $table      = 'unidades';
+    protected $primaryKey = 'id';
+
+    protected $useAutoIncrement = true;
+
+    protected $returnType     = 'object';
+    //eliminacion de filas
+    protected $useSoftDeletes = false;
+
+    protected $allowedFields = ['name', 'short_name', 'estado'];
+
+    protected $useTimestamps = true;
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    protected $validationRules    = [];
+    protected $validationMessages = [];
+    protected $skipValidation     = false;
+}
