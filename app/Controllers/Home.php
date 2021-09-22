0aa28a00d6c60ba2ff45bb4c41ce6e7785b8ed26
tucno21 <carlitostucno@gmail.com>
2021-09-22 15:43:36 -0500

    base modificada sb_admin

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,6 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('page');
+        return view('backend/dashboard/index');
     }
 }
---
+++ app/Views/backend/dashboard/index.php
@@ -0,0 +1,25 @@
+<?php
+include '../app/Views/backend/sb_admin/head.php';
+?>
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-2 text-gray-800">Dashboard</h1>
+
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">MOdelo o bas</h6>
+        </div>
+        <div class="card-body">
+
+
+        </div>
+    </div>
+
+</div>
+<!-- /.container-fluid -->
+<?php
+include '../app/Views/backend/sb_admin/footer.php';
+?>
