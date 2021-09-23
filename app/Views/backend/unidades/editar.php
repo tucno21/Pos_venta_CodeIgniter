8de5d65af7edafe0b76eec6eb779da81d9328d99
tucno21 <carlitostucno@gmail.com>
2021-09-23 09:10:48 -0500

    validar editar unidad

--- app/Views/backend/unidades/editar.php
+++ app/Views/backend/unidades/editar.php
@@ -1,7 +1,7 @@
 <?= $template['head'] ?>
 <!-- Begin Page Content -->
 <div class="container-fluid">
-
+    <?php $validation =  \Config\Services::validation(); ?>
     <!-- Page Heading -->
     <h1 class="mb-2 text-gray-800 h3">Editar Unidad</h1>
 
@@ -11,16 +11,27 @@
             <a href="<?php echo base_url(); ?>/unidades" class="btn btn-info">Volver</a>
         </div>
         <form action="<?php echo base_url(); ?>/unidades/actualizar" method="post" autocomplete="off">
+            <?= csrf_field() ?>
             <div class="card-body">
                 <div class="form-group">
                     <div class="row">
                         <div class="col-12 col-sm-6">
-                            <label for="name">Nombre</label>
-                            <input id="name" class="form-control" type="text" name="name" value="<?php echo $unidad->name; ?>" autofocus required>
+                            <label class="form-label">Nombre</label>
+                            <input type="text" class="form-control <?php if ($validation->getError('name')) : ?>is-invalid<?php endif ?>" name="name" value="<?php echo $unidad->name; ?>" autofocus required />
+                            <?php if ($validation->getError('name')) : ?>
+                                <div class="invalid-feedback">
+                                    <?= $validation->getError('name') ?>
+                                </div>
+                            <?php endif; ?>
                         </div>
                         <div class="col-12 col-sm-6">
-                            <label for="short_name">Nombre Corto</label>
-                            <input id="short_name" class="form-control" type="text" name="short_name" value="<?php echo $unidad->short_name; ?>" autofocus required>
+                            <label class="form-label">Nombre Corto</label>
+                            <input type="text" class="form-control <?php if ($validation->getError('short_name')) : ?>is-invalid<?php endif ?>" name="short_name" value="<?php echo $unidad->short_name; ?>" autofocus required />
+                            <?php if ($validation->getError('short_name')) : ?>
+                                <div class="invalid-feedback">
+                                    <?= $validation->getError('short_name') ?>
+                                </div>
+                            <?php endif; ?>
                         </div>
                         <input type="hidden" name="id" value="<?php echo $unidad->id; ?>">
                     </div>
