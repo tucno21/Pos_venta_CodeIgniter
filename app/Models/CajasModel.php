42491ef1f03e0a167328238e59e1a80206b3477f
tucno21 <carlitostucno@gmail.com>
2021-09-23 17:15:40 -0500

    modelo caja y roles

---
+++ app/Models/CajasModel.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class CajasModel extends Model
+{
+    protected $table      = 'cajas';
+    protected $primaryKey = 'id';
+
+    protected $useAutoIncrement = true;
+
+    protected $returnType     = 'object';
+    //eliminacion de filas
+    protected $useSoftDeletes = false;
+
+    protected $allowedFields = ['numero_caja', 'name', 'folio', 'estado'];
+
+    protected $useTimestamps = true;
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    protected $validationRules    = [];
+    protected $validationMessages = [];
+    protected $skipValidation     = false;
+}
