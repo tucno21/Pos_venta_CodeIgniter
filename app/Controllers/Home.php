4452ff17bad65fc307107cd99182c35101c7ed04
tucno21 <carlitostucno@gmail.com>
2021-09-24 05:22:52 -0500

    login finalizado

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,63 @@
 
 namespace App\Controllers;
 
+use App\Models\UsuariosModel;
+
 class Home extends BaseController
 {
+    protected $usuarios;
+
+    public function __construct()
+    {
+        helper(['form', 'url']);
+        $this->usuarios = new UsuariosModel();
+    }
+
     public function index()
     {
-        return view('backend/dashboard/index');
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            $inputs = $this->validate([
+                'username' => 'required',
+                'password' => 'required',
+            ]);
+
+            if (!$inputs) {
+                return view('login', [
+                    'validation' => $this->validator,
+                ]);
+            } else {
+
+                $username = $this->request->getPost('username');
+                $password = $this->request->getPost('password');
+
+                $usuario = $this->usuarios->where('username', $username)->first();
+
+                if (isset($usuario)) {
+                    $encritar = password_verify($password, $usuario->password);
+
+                    if ($encritar) {
+                        return redirect()->to(base_url() . '/dashboard');
+                    } else {
+                        $errorPass = 'Erro de contraseña';
+                        return view('login', [
+                            'errorPass' => $errorPass,
+                        ]);
+                    }
+                } else {
+                    $errorName = 'El usuario no existe';
+                    return view('login', [
+                        'errorName' => $errorName,
+                    ]);
+                }
+            }
+        } else {
+            return view('login', []);
+        }
     }
+
+    // public function vlogin()
+    // {
+
+    // }
 }
--- app/Views/login.php
+++ app/Views/login.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
 
@@ -9,16 +9,14 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Login</title>
+    <title>Login</title>
 
     <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="../css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 
@@ -28,54 +26,54 @@
 
         <!-- Outer Row -->
         <div class="row justify-content-center">
-
+            <?php $validation =  \Config\Services::validation(); ?>
             <div class="col-xl-10 col-lg-12 col-md-9">
 
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
+                <div class="my-5 border-0 shadow-lg card o-hidden">
+                    <div class="p-0 card-body">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                            <div class="col-lg-6">
+
+                            <div class="col">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                        <h1 class="mb-4 text-gray-900 h4">Bienvenido</h1>
                                     </div>
-                                    <form class="user">
+                                    <form class="user" method="post" action="<?php echo base_url(); ?>">
+                                        <?= csrf_field() ?>
                                         <div class="form-group">
-                                            <input type="email" class="form-control form-control-user"
-                                                id="exampleInputEmail" aria-describedby="emailHelp"
-                                                placeholder="Enter Email Address...">
+                                            <input type="text" name="username" class="form-control form-control-user <?php if ($validation->getError('username')) : ?>is-invalid<?php endif ?>" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Ingresa el ususario...">
+
+                                            <?php if ($validation->getError('username')) : ?>
+                                                <div class="invalid-feedback">
+                                                    <?= $validation->getError('username') ?>
+                                                </div>
+                                            <?php endif; ?>
+
+                                            <?php if (isset($errorName)) : ?>
+                                                <div class="alert alert-danger">
+                                                    <?php echo $errorName; ?>
+                                                </div>
+                                            <?php endif; ?>
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" class="form-control form-control-user"
-                                                id="exampleInputPassword" placeholder="Password">
+                                            <input type="password" name="password" class="form-control form-control-user <?php if ($validation->getError('password')) : ?>is-invalid<?php endif ?>" id="exampleInputPassword" placeholder="Ingresa tu contraseña">
+
+                                            <?php if (isset($validation)) : ?>
+                                                <div class="invalid-feedback">
+                                                    <?= $validation->getError('password') ?>
+                                                </div>
+                                            <?php endif; ?>
+
+                                            <?php if (isset($errorPass)) : ?>
+                                                <div class="alert alert-danger">
+                                                    <?php echo $errorPass; ?>
+                                                </div>
+                                            <?php endif; ?>
                                         </div>
-                                        <div class="form-group">
-                                            <div class="custom-control custom-checkbox small">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <label class="custom-control-label" for="customCheck">Remember
-                                                    Me</label>
-                                            </div>
-                                        </div>
-                                        <a href="index.html" class="btn btn-primary btn-user btn-block">
-                                            Login
-                                        </a>
-                                        <hr>
-                                        <a href="index.html" class="btn btn-google btn-user btn-block">
-                                            <i class="fab fa-google fa-fw"></i> Login with Google
-                                        </a>
-                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                        </a>
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">Entrar</button>
                                     </form>
                                     <hr>
-                                    <div class="text-center">
-                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
-                                    </div>
-                                    <div class="text-center">
-                                        <a class="small" href="register.html">Create an Account!</a>
-                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -89,14 +87,14 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../vendor/jquery/jquery.min.js"></script>
+    <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
+    <script src="../js/sb-admin-2.min.js"></script>
 
 </body>
 
