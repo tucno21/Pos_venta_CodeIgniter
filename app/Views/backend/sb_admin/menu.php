ab470c680e45d7e0b100ab36856b2a2dfd3e30b0
tucno21 <carlitostucno@gmail.com>
2021-09-24 08:31:31 -0500

    cambio de menu lateral

--- app/Views/backend/sb_admin/menu.php
+++ app/Views/backend/sb_admin/menu.php
@@ -4,42 +4,64 @@
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
-            <!-- Divider -->
-            <hr class="sidebar-divider">
             <!-- Nav Item - Pages Collapse Menu -->
             <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseA" aria-expanded="true" aria-controls="collapseA">
                     <i class="fas fa-box-open"></i>
                     <span>Productos</span>
                 </a>
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
+                <div id="collapseA" class="collapse" aria-labelledby="headingA" data-parent="#accordionSidebar">
+                    <div class="py-2 bg-white rounded collapse-inner">
                         <a class="collapse-item" href="/productos">Productos</a>
                         <a class="collapse-item" href="/unidades">Unidades</a>
                         <a class="collapse-item" href="/categorias">Categorias</a>
                     </div>
                 </div>
             </li>
-            <!-- Divider -->
-            <hr class="sidebar-divider">
             <!-- Nav Item - Charts -->
             <li class="nav-item">
                 <a class="nav-link" href="/clientes">
                     <i class="fas fa-users"></i>
                     <span>Clientes</span></a>
             </li>
-            <hr class="sidebar-divider">
-            <!-- Nav Item - Pages Collapse Menu -->
+
             <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseC" aria-expanded="true" aria-controls="collapseC">
-                    <i class="fas fa-tools"></i>
-                    <span>Administración</span>
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseB" aria-expanded="true" aria-controls="collapseB">
+                    <i class="fas fa-truck"></i>
+                    <span>Compras</span>
                 </a>
-                <div id="collapseC" class="collapse" aria-labelledby="headingC" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <a class="collapse-item" href="/configuraciones">Configuración</a>
-                        <a class="collapse-item" href="/usuarios">Usuarios</a>
-                        <a class="collapse-item" href="/categorias">..</a>
+                <div id="collapseB" class="collapse" aria-labelledby="headingB" data-parent="#accordionSidebar">
+                    <div class="py-2 bg-white rounded collapse-inner">
+                        <a class="collapse-item" href="/compras/nuevo">Nueva Compra</a>
+                        <a class="collapse-item" href="/compras">Compras</a>
+                    </div>
+                </div>
+            </li>
+
+            <!-- Nav Item - Charts -->
+            <li class="nav-item">
+                <a class="nav-link" href="/cajas">
+                    <i class="far fa-money-bill-alt"></i>
+                    <span>Caja</span></a>
+            </li>
+
+            <!-- Nav Item - Charts -->
+            <li class="nav-item">
+                <a class="nav-link" href="/ventas">
+                    <i class="fas fa-cart-arrow-down"></i>
+                    <span>Ventas</span></a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseD" aria-expanded="true" aria-controls="collapseD">
+                    <i class="fas fa-tools"></i>
+                    <span>Reportes</span>
+                </a>
+                <div id="collapseD" class="collapse" aria-labelledby="headingD" data-parent="#accordionSidebar">
+                    <div class="py-2 bg-white rounded collapse-inner">
+                        <a class="collapse-item" href="/reportes/ventas">Reporte de ventas</a>
+                        <a class="collapse-item" href="/reportes/productos">Reporte de Productos</a>
+                        <a class="collapse-item" href="/reportes/procat">Productos x categoria</a>
                     </div>
                 </div>
             </li>
@@ -47,41 +69,19 @@
             <!-- Divider -->
             <hr class="sidebar-divider">
 
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                TITULO 2
-            </div>
-
-            <!-- Nav Item - Pages Collapse Menu -->
             <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-                    <i class="fas fa-fw fa-folder"></i>
-                    <span>Pages</span>
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseC" aria-expanded="true" aria-controls="collapseC">
+                    <i class="fas fa-tools"></i>
+                    <span>Administración</span>
                 </a>
-                <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Login Screens:</h6>
-                        <a class="collapse-item" href="login.html">Login</a>
-                        <a class="collapse-item" href="register.html">Register</a>
-                        <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                        <div class="collapse-divider"></div>
-                        <h6 class="collapse-header">Other Pages:</h6>
-                        <a class="collapse-item" href="404.html">404 Page</a>
-                        <a class="collapse-item" href="blank.html">Blank Page</a>
+                <div id="collapseC" class="collapse" aria-labelledby="headingC" data-parent="#accordionSidebar">
+                    <div class="py-2 bg-white rounded collapse-inner">
+                        <a class="collapse-item" href="/configuraciones">Configuración</a>
+                        <a class="collapse-item" href="/usuarios">Usuarios</a>
+                        <a class="collapse-item" href="/roles">Roles</a>
+                        <a class="collapse-item" href="/cajas">Cajas</a>
+                        <a class="collapse-item" href="/logs">Logs de accesos</a>
+                        <a class="collapse-item" href="/respaldo">Respaldo</a>
                     </div>
                 </div>
-            </li>
-
-            <!-- Nav Item - Charts -->
-            <li class="nav-item">
-                <a class="nav-link" href="charts.html">
-                    <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Enlace 1</span></a>
-            </li>
-
-            <!-- Nav Item - Tables -->
-            <li class="nav-item active">
-                <a class="nav-link" href="tables.html">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Enlace 2</span></a>
             </li>
