3d3603e34fa85156452b3b572481539b65c54320
tucno21 <carlitostucno@gmail.com>
2021-10-03 14:51:51 -0500

    traductor datatable

--- public/js/demo/datatables-demo.js
+++ public/js/demo/datatables-demo.js
@@ -1,4 +1,17 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  $('#dataTable').DataTable({
+      "language": {
+        "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
+    },
+  });
+});
+
+$(document).ready(function() {
+  $('#dataTableVentas').DataTable({
+      "language": {
+        "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
+    },
+    order: [0, 'desc'],
+  });
 });
