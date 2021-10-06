d45257fa22aff0d6579236d3452ddc2bf1c23dc1
tucno21 <carlitostucno@gmail.com>
2021-10-06 16:59:13 -0500

    dashboard

--- app/Views/backend/sb_admin/head.php
+++ app/Views/backend/sb_admin/head.php
@@ -44,7 +44,7 @@ $name = $session->name;
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/dashboard">
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
